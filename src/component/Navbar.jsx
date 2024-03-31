a13917854e7008c80ad860b00fa6b02e57adab19
Uriadutu <balisoan03@gmail.com>
2024-04-01 06:45:31 +0800

    commit ke lupa 3

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import LogoAPK from "../img/LogoApk.png";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   const location = useLocation(); // Gunakan useLocation untuk mendapatkan lokasi saat ini
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
   const [activeLink, setActiveLink] = useState("");
 
+  // Fungsi untuk menangani klik pada tombol hamburger
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
+
+  // Set activeLink berdasarkan path saat ini
   useEffect(() => {
-    // Set activeLink berdasarkan path saat ini
     if (location.pathname === "/") {
       setActiveLink("beranda");
     } else if (location.pathname === "/bantuan") {
@@ -24,7 +31,11 @@ const Navbar = () => {
       <NavLink to="/" exact className="text-blue-500">
         <img src={LogoAPK} alt="Logo" className="h-10" />
       </NavLink>
-      <div className="flex space-x-4 text-gray">
+      <div
+        className={`flex space-x-4 text-gray ${
+          isMobileNavOpen ? "block" : "hidden"
+        } lg:flex lg:space-x-4`}
+      >
         <NavLink
           to="/"
           exact
@@ -46,8 +57,28 @@ const Navbar = () => {
         </NavLink>
       </div>
       <NavLink to="/login">
-        <h1 className="text-white bg-blue-500 px-2 py-1 rounded">LOGIN</h1>
+        <h1 className="text-white bg-blue-500 px-2 py-1 rounded hidden lg:block">
+          LOGIN
+        </h1>
       </NavLink>
+      {/* Tombol hamburger */}
+      <button className="block lg:hidden" onClick={toggleMobileNav}>
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16m-7 6h7"
+          />
+        </svg>
+      </button>
+      {/* Tampilkan menu navigasi mobile saat isMobileNavOpen true */}
+      {isMobileNavOpen && <MobileNav toggleMobileNav={toggleMobileNav} />}
     </div>
   );
 };
--- src/component/Welcome.jsx
+++ src/component/Welcome.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import {
   Bar,
   BarChart,
@@ -10,14 +10,12 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
-import axios from "axios"
+import axios from "axios";
 import { Link } from "react-router-dom";
-
 
 import BGMID from "../img/MidBG.png";
 
 const Table = ({ data }) => {
-
   return (
     <table className="min-w-full divide-y divide-gray-200">
       <thead className="bg-gray-50">
@@ -47,8 +45,10 @@ const Table = ({ data }) => {
           <tr key={item.name}>
             <td className="px-6 py-4 whitespace-nowrap">
               <div className="flex items-center">
-              <div className="text-sm font-medium text-gray-900">{index + 1}</div>
+                <div className="text-sm font-medium text-gray-900">
+                  {index + 1}
                 </div>
+              </div>
             </td>
             <td className="px-6 py-4 whitespace-nowrap">
               <div className="flex items-center">
@@ -66,10 +66,9 @@ const Table = ({ data }) => {
     </table>
   );
 };
-const Welcome = () => {
 
+const Welcome = () => {
   const data = [
-    
     {
       name: "Marah",
       Marah: "29",
@@ -94,15 +93,16 @@ const Welcome = () => {
       name: "Netral",
       Netral: "10",
     },
-    
   ];
+
   const [activeTab, setActiveTab] = useState("grafik");
-  
   const [kui, setKui] = useState([]);
 
   const getKui = async () => {
     try {
-      const response = await axios.get("http://localhost:5011/kuisioner/data/baru");
+      const response = await axios.get(
+        "http://localhost:5011/kuisioner/data/baru"
+      );
       setKui(response.data);
     } catch (error) {
       console.log(error);
@@ -112,63 +112,62 @@ const Welcome = () => {
   useEffect(() => {
     getKui();
   }, []);
+
   return (
     <div>
-      <div className="w-full h-[100vh]">
-        <div className="flex items-center h-10 intro-y">
+      <div className="w-full h-full md:flex md:flex-col md:justify-between">
+        <div className="md:py-10 md:pl-10 md:pr-4 md:border-b md:border-gray-200">
           <h6 className="text-blue-800 font-bold">Beranda</h6>
+          <h1 className="text-3xl font-bold text-gray-800">
+            Emosi Manusia Terhadap Dampak Teknologi AI Bulan Agustus 2024
+          </h1>
+          <div className="mt-10 flex flex-wrap justify-between md:flex md:flex-row md:mt-0">
+            <button
+              className={`mr-5 px-4 py-2 focus:outline-none ${
+                activeTab === "grafik"
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-200 text-gray-800"
+              }`}
+              onClick={() => setActiveTab("grafik")}
+            >
+              Grafik
+            </button>
+            <button
+              className={`px-4 py-2 focus:outline-none ${
+                activeTab === "tabel"
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-200 text-gray-800"
+              }`}
+              onClick={() => setActiveTab("tabel")}
+            >
+              Tabel
+            </button>
+          </div>
         </div>
-        <h1 className="text-3xl font-bold text-gray-800">
-          Emosi Manusia Terhadap Dampak Teknologi AI Bulan Agustus 2024
-        </h1>
-        <div className="flex mt-10">
-          <button
-            className={`mr-5 px-4 py-2 focus:outline-none ${
-              activeTab === "grafik"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-200 text-gray-800"
-            }`}
-            onClick={() => setActiveTab("grafik")}
-          >
-            Grafik
-          </button>
-          <button
-            className={`px-4 py-2 focus:outline-none ${
-              activeTab === "tabel"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-200 text-gray-800"
-            }`}
-            onClick={() => setActiveTab("tabel")}
-          >
-            Tabel
-          </button>
-        </div>
-        <div className="flex">
+        <div className="md:flex md:items-start md:mt-5">
           {activeTab === "grafik" && (
-            <div className="lg:w-1/2">
+            <div className="md:w-1/2">
               <h3 className="text-xl font-bold text-gray-800 mb-5">
                 Data Emosi dalam Bentuk Grafik
               </h3>
-              <div className="">
-                <div className="overflow-hidden border border-gray-200 rounded-lg overflow-x-auto">
-                  <BarChart width={730} height={400} data={data}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Bar dataKey="Marah" fill="#4682b4" barSize={1000} />
-                    <Bar dataKey="Terkejut" fill="#4682b4" barSize={1000} />
-                    <Bar dataKey="Takut" fill="#4682b4" barSize={1000} />
-                    <Bar dataKey="Sedih" fill="#4682b4" barSize={1000} />
-                    <Bar dataKey="Senang" fill="#4682b4" barSize={1000} />
-                    <Bar dataKey="Netral" fill="#4682b4" barSize={1000} />
-                  </BarChart>
-                </div>
+              <div className="overflow-x-auto md:max-w-full">
+                <BarChart width={730} height={400} data={data}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Bar dataKey="Marah" fill="#4682b4" barSize={1000} />
+                  <Bar dataKey="Terkejut" fill="#4682b4" barSize={1000} />
+                  <Bar dataKey="Takut" fill="#4682b4" barSize={1000} />
+                  <Bar dataKey="Sedih" fill="#4682b4" barSize={1000} />
+                  <Bar dataKey="Senang" fill="#4682b4" barSize={1000} />
+                  <Bar dataKey="Netral" fill="#4682b4" barSize={1000} />
+                </BarChart>
               </div>
             </div>
           )}
           {activeTab === "tabel" && (
-            <div className="lg:w-1/2">
+            <div className="md:w-1/2">
               <h3 className="text-xl font-bold text-gray-800 mb-5">
                 Data Emosi dalam Bentuk Tabel
               </h3>
@@ -177,35 +176,25 @@ const Welcome = () => {
               </div>
             </div>
           )}
-          <div className="lg:w-1/2 pl-11">
-
-          <div className="mt-12">
-            <div className="">
-              <div className="h-full">
-                <div className="flex bg-blue-500 p-4 rounded-t-lg justify-center">
-                  <h1 className="text-white font-bold text-xl">
-                    Isi Kuesioner
-                  </h1>
-                </div>
-                <div className="bg-white p-4 shadow-lg rounded-b-lg ">
-                  
-                      <div className="">
-                        <Link to={kui && kui.Linkkui}>
-                          <h1 className="truncate">{kui && kui.Linkkui}</h1>
-                        </Link>
-                      </div>
+          <div className="md:w-1/2 md:pl-5">
+            <div className="mt-12">
+              <div className="flex bg-blue-500 p-4 rounded-t-lg justify-center">
+                <h1 className="text-white font-bold text-xl">Isi Kuesioner</h1>
+              </div>
+              <div className="bg-white p-4 shadow-lg rounded-b-lg ">
+                <div className="">
+                  <Link to={kui && kui.Linkkui}>
+                    <h1 className="truncate">{kui && kui.Linkkui}</h1>
+                  </Link>
                 </div>
               </div>
             </div>
             <div className=" mt-5">
-              <div className="h-full">
-                <div className="flex bg-blue-500 p-4 rounded-t-lg justify-center">
-                  <h1 className="text-white font-bold text-xl">Download</h1>
-                </div>
-                <div className="flex bg-white p-4 shadow-lg rounded-b-lg"></div>
+              <div className="flex bg-blue-500 p-4 rounded-t-lg justify-center">
+                <h1 className="text-white font-bold text-xl">Download</h1>
               </div>
+              <div className="bg-white p-4 shadow-lg rounded-b-lg"></div>
             </div>
-          </div>
           </div>
         </div>
       </div>
@@ -217,9 +206,8 @@ const Welcome = () => {
           backgroundPosition: "center",
         }}
       ></div>
-      {/* <div className="w-full h-[100vh]"></div> */}
     </div>
   );
-}
+};
 
-export default Welcome
+export default Welcome;
